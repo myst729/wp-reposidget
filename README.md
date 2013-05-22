b7eca6702f2c462533614c21ac266858d944dbe9
Leo Deng <myst.dg@gmail.com>
2013-05-22 22:17:02 +0800

    first drop

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ WP Reposidget (GitHub 项目挂件)
 =============================
 
 Contributors: Leo Deng ([@米粽粽](http://weibo.com/myst729))  
-Plugin URI: http://forcefront.com/wp-reposidget-plugin/  
+Plugin URI: http://forcefront.com/reposidget-plugin/  
 Tags: github, reposidget  
 Requires at least: 2.7.0  
 Tested up to: 3.5.1  
@@ -10,3 +10,54 @@ Stable tag: 1.0.0
 Author URI: http://forcefront.com/  
 Donate link: http://me.alipay.com/myst  
 License: GPLv2 or later
+
+WordPress Reposidget plugin - insert GitHub repository widget on you site.
+
+
+###Description###
+
+Insert [GitHub](https://github.com/) repository widget on you site.
+
+在 WordPress 文章/页面中插入 [GitHub](https://github.com/) 项目挂件。
+
+
+###Installation###
+
+1. Upload the plugin to your `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Now you could see the `GitHub Repo` button in post/page editor.
+4. Input the path to your GitHub repo: `username/repo`.
+
+上传插件到您的 WordPress 插件目录，登录后台激活即可。进入文章编辑界面，您就会看到“GitHub Repo”的快捷按钮。输入您的项目路径：“用户名/项目”即可。
+
+
+###Frequently Asked Questions###
+
+Looking forward to your questions.
+
+期待您的提问。
+
+
+###Donation###
+
+If you like this plugin and want to thank its developer, you may consider make a donation via [**AliPay**](https://me.alipay.com/myst).
+
+如果您觉得这个插件还不错，想对开发者表示感谢，可以考虑通过 [**支付宝个人收款页**](https://me.alipay.com/myst) 打赏开发者一点儿银子。
+
+
+###Screenshots###
+
+1. Use shortcode to insert reposidget into the post/page.
+2. The look of a reposidget.
+
+
+###Changelog###
+
+= 1.0.0 (2013-05-22) =
+* 发布第一个版本
+
+
+###Upgrade Notice###
+
+= 1.0.0 =  
+向 WordPress 文章/页面中插入 GitHub 项目挂件。
---
+++ wp-reposidget.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: WP Reposidget (GitHub 项目挂件)
+Plugin URI: http://forcefront.com/reposidget-plugin/
+Description: WordPress Reposidget plugin - insert GitHub repository widget on you site. 在 WordPress 文章/页面中插入 GitHub 项目挂件。
+Version: 1.0.0
+Author: Leo Deng (@米粽粽)
+Author URI: http://forcefront.com/
+License: GPLv2 or later
+*/
+
+
+function multi_lingua() {
+    load_plugin_textdomain('repo', false, dirname(plugin_basename(__FILE__)) . '/lang/');
+}
+
+function quicktags() { ?>
+    <script type="text/javascript">
+        function repoPath(e, c, ed) {
+            var path = prompt('<?php _e("Path to the repo you want to insert:", "repo"); ?>');
+            if(!!path) {
+                this.tagStart = '[repo path="' + path + '"]';
+                QTags.TagButton.prototype.callback.call(this, e, c, ed);
+            }
+        }
+        QTags.addButton('repo', 'GitHub Repo', repoPath);
+    </script><?php
+}
+
+function add_reposidget_stylesheet() {
+    echo "\n" . '<link rel="stylesheet" href="' . plugins_url('wp-reposidget.css', __FILE__) . '" />' . "\n";
+}
+
+function get_repo($path) {
+    $url = "https://api.github.com/repos/" . $path;
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36");
+    $repo = json_decode(curl_exec($ch), true);
+    curl_close ($ch);
+    return $repo;
+}
+
+function reposidget($atts) {
+    $path = $atts["path"];
+    $repo = get_repo($path);
+    if($repo["description"] != "" && $repo["homepage"] != null) {
+        $repoContent = '<p>' . $repo["description"] . '</p><p class="homepage"><strong><a href="' . $repo["homepage"] . '">' . $repo["homepage"] . '</a></strong></p>';
+    } else if($repo["description"] != "") {
+        $repoContent = '<p>' . $repo["description"] . '</p>';
+    } else if($repo["homepage"] != null) {
+        $repoContent = '<p class="homepage"><strong><a href="' . $repo["homepage"] . '">' . $repo["homepage"] . '</a></strong></p>';
+    } else {
+        $repoContent = '<p class="none">No description or homepage.</p>';
+    }
+    $html = '<div class="reposidget"><div class="reposidget-header"><h2><a href="https://github.com/' . $repo["owner"]["login"] . '">' . $repo["owner"]["login"] . '</a>&nbsp;/&nbsp;<strong><a href="' . $repo["html_url"] . '">' . $repo["name"] .  '</a></strong></h2></div><div class="reposidget-content">' . $repoContent . '</div><div class="reposidget-footer"><span class="social"><span class="star">' . $repo["watchers_count"] . '</span><span class="fork">' . $repo["forks_count"] . '</span></span><a href="' . $repo["html_url"] . '/archive/' . $repo["master_branch"] . '.zip">Download as zip</a></div></div>';
+    return $html;
+}
+
+add_action('admin_print_footer_scripts', 'quicktags');
+add_action('plugins_loaded', 'multi_lingua');
+add_action('wp_head', 'add_reposidget_stylesheet');
+add_shortcode('repo', 'reposidget');
+
+?>
